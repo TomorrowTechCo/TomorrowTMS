b764b4cc02252668479df20185972648f7f382e2
Luischv <luischa123@gmail.com>
2020-01-10 15:45:52 -0500

    Add Purescript support
    Este commit agrega soporte a módulos en Purescript, específicamente,
    a través del framework purescript-react-basic, que compila a componentes
    de React que pueden utilizarse directamente.

--- src/javascript/App.js
+++ src/javascript/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { jsCounter as Counter } from "../output/Counter.Interop";
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +21,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <Counter label="Dinero"/>
+      <Counter />
     </div>
   );
 }
